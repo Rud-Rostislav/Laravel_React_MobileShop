b6844da6ffd9ab09d946fd1217275f6de8470671
Rud-Rostislav <rudrost.se@gmail.com>
2024-06-30 03:59:38 +0300

    Photo names edit

--- app/Http/Controllers/Shop/ProductController.php
+++ app/Http/Controllers/Shop/ProductController.php
@@ -27,34 +27,6 @@ class ProductController extends Controller
         return Inertia::render('Products/Create');
     }
 
-    public function store(Request $request): RedirectResponse
-    {
-        $request->validate([
-            'name' => 'required',
-            'description' => 'required',
-            'price' => 'required',
-            'quantity' => 'required',
-            'photos.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048'
-        ]);
-
-        $product = new Product();
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->quantity = $request->quantity;
-        $product->save();
-
-        $productFolder = 'product_photos/' . $product->id;
-
-        if ($request->hasFile('photos')) {
-            foreach ($request->file('photos') as $index => $photo) {
-                $path = $photo->storeAs($productFolder, 'product_' . $product->id . '_photo_' . ($index + 1) . '.' . $photo->getClientOriginalExtension(), 'public');
-                $product->photos()->create(['path' => $path]);
-            }
-        }
-
-        return redirect()->route('products.index');
-    }
 
     public function show(Product $product): Response
     {
@@ -64,6 +36,7 @@ class ProductController extends Controller
         ]);
     }
 
+
     public function edit(Product $product): Response
     {
         $product->load('photos');
@@ -72,7 +45,7 @@ class ProductController extends Controller
         ]);
     }
 
-    public function update(Request $request, Product $product): RedirectResponse
+    public function storeOrUpdate(Request $request, Product $product = null): RedirectResponse
     {
         $request->validate([
             'name' => 'required',
@@ -82,12 +55,40 @@ class ProductController extends Controller
             'photos.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048'
         ]);
 
+        if (!$product) {
+            $product = new Product();
+        }
+
         $product->name = $request->name;
         $product->description = $request->description;
         $product->price = $request->price;
         $product->quantity = $request->quantity;
-
         $product->save();
+
+        $productFolder = 'product_photos/' . $product->id;
+
+        if ($request->hasFile('photos')) {
+            $existingPhotos = $product->photos->pluck('path')->toArray();
+
+            foreach ($request->file('photos') as $index => $photo) {
+                $fileName =  'photo_' . ($index + 1) . '.' . $photo->getClientOriginalExtension();
+                $folderPath = 'photos/product_' . $product->id;
+
+                if (!Storage::disk('public')->exists($folderPath)) {
+                    Storage::disk('public')->makeDirectory($folderPath);
+                }
+
+                $path = $photo->storeAs($folderPath, $fileName, 'public');
+
+                if (in_array($fileName, $existingPhotos)) {
+                    Storage::delete('public/' . $path);
+                    $existingPhoto = $product->photos()->where('path', 'like', '%' . $fileName)->first();
+                    $existingPhoto->update(['path' => $path]);
+                } else {
+                    $product->photos()->create(['path' => $path]);
+                }
+            }
+        }
 
         if ($request->has('deleted_photos')) {
             $deletedPhotos = $product->photos()->whereIn('id', $request->input('deleted_photos'))->get();
@@ -97,24 +98,17 @@ class ProductController extends Controller
             }
         }
 
-        if ($request->hasFile('photos')) {
-            $existingPhotos = $product->photos->pluck('path')->toArray();
-
-            foreach ($request->file('photos') as $index => $photo) {
-                $fileName = 'product_' . $product->id . '_photo_' . ($index + 1) . '.' . $photo->getClientOriginalExtension();
-                $path = $photo->storeAs('product_photos', $fileName, 'public');
-
-                if (in_array($fileName, $existingPhotos)) {
-                    Storage::delete('public/product_photos/' . $fileName);
-                    $existingPhoto = $product->photos()->where('path', 'like', '%' . $fileName)->first();
-                    $existingPhoto->update(['path' => $path]);
-                } else {
-                    $product->photos()->create(['path' => $path]);
-                }
-            }
-        }
-
         return redirect()->route('products.show', $product);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        return $this->storeOrUpdate($request);
+    }
+
+    public function update(Request $request, Product $product): RedirectResponse
+    {
+        return $this->storeOrUpdate($request, $product);
     }
 
     public function destroy(Product $product): Application|Redirector|RedirectResponse|\Illuminate\Contracts\Foundation\Application
