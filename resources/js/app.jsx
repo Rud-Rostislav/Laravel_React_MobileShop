8ea7a977f9a37cdf715e5166864fbbb6ecf657df
Rud-Rostislav <rudrost.se@gmail.com>
2024-07-03 17:32:10 +0300

    Reorganize css

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,24 @@
 import './bootstrap';
-import '../css/app.css';
+import '../css/basket.css';
+import '../css/burger_menu.css';
+import '../css/main.css';
+import '../css/media_screen.css';
+import '../css/orders.css';
+import '../css/product_create_edit.css';
+import '../css/product_index.css';
+import '../css/product_show.css';
+import '../css/project_task.css';
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {createRoot} from 'react-dom/client';
+import {createInertiaApp} from '@inertiajs/react';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
+    setup({el, App, props}) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
