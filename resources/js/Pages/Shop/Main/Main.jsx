a47089bbb7958e620973b3a7d0af936dc5f2757d
Rud-Rostislav <rudrost.se@gmail.com>
2024-10-13 13:34:21 +0300

    Edit animations

--- resources/js/Pages/Shop/Main/Main.jsx
+++ resources/js/Pages/Shop/Main/Main.jsx
@@ -19,43 +19,36 @@ const Main = ({basket, products}) => {
 
                 <div className='container_1'>
                     <motion.div
-                        initial={{opacity: 0, x: '-10vw'}}
-                        whileInView={{opacity: 1, x: 0}}
+                        initial={{y: '-5vw'}}
+                        whileInView={{y: 0}}
                         transition={{duration: 5}}
                     >
                         <img src={"images/main/1.png"} alt="Image"/>
                     </motion.div>
                     <motion.div
-                        initial={{opacity: 0, x: '10vw'}}
-                        whileInView={{opacity: 1, x: 0}}
+                        initial={{y: '5vw'}}
+                        whileInView={{y: 0}}
                         transition={{duration: 5}}
                     >
                         <p>Новий iPhone 16 Pro Max</p>
                     </motion.div>
-
                 </div>
 
                 <div className='container_2'>
                     <motion.div
-                        initial={{opacity: 0, x: '-20vw'}}
-                        whileInView={{opacity: 1, x: 0}}
-                        transition={{duration: 2}}
+                        initial={{x: '15vw'}}
+                        whileInView={{x: 0}}
+                        transition={{duration: 5}}
                     >
                         <img className='img_container_2' src={"images/main/2.png"} alt="Image"/>
                     </motion.div>
 
-                    <motion.div
-                        initial={{marginTop: 0}}
-                        whileInView={{marginTop: 100}}
-                        transition={{duration: 2}}
-                    >
-                        <p className='p_container_2'>Вибір техніки на будь який смак</p>
-                    </motion.div>
+                    <p className='p_container_2'>Вибір техніки на будь який смак</p>
 
                     <motion.div
                         initial={{x: '-75vw'}}
                         whileInView={{x: 0}}
-                        transition={{duration: 2}}
+                        transition={{duration: 3}}
                     >
                         <div className="products">
                             {products.map((product) => (
@@ -83,13 +76,7 @@ const Main = ({basket, products}) => {
                         </div>
                     </motion.div>
 
-                    <motion.div
-                        initial={{opacity: 0, y: 25}}
-                        whileInView={{opacity: 1, y: 0}}
-                        transition={{duration: 2}}
-                    >
-                        <Link className="shop_button" href={route('products.index')}>Переглянути все</Link>
-                    </motion.div>
+                    <Link className="shop_button" href={route('products.index')}>Переглянути все</Link>
                 </div>
             </main>
 
